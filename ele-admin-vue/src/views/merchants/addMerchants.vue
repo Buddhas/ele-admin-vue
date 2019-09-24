3ee1ffa83ff5219fef14aeee8e7dcf171a45b689
chenbin <2424932515@qq.com>
2019-09-24 15:36:24 +0800

    添加商铺组件开发

--- ele-admin-vue/src/views/merchants/addMerchants.vue
+++ ele-admin-vue/src/views/merchants/addMerchants.vue
@@ -4,55 +4,100 @@
       ref="ruleForm"
       :model="ruleForm"
       :rules="rules"
-      label-width="100px"
+      label-width="200px"
       class="demo-ruleForm"
     >
-      <el-form-item label="活动名称" prop="name">
-        <el-input v-model="ruleForm.name" class="w350" />
+      <el-form-item label="商铺名称" prop="name">
+        <el-input v-model="ruleForm.name" class="w500" />
       </el-form-item>
-      <el-form-item label="活动区域" prop="region">
-        <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai" />
-          <el-option label="区域二" value="beijing" />
-        </el-select>
+      <el-form-item label="详细地址" prop="address">
+        <el-input v-model="ruleForm.address" class="w500" />
       </el-form-item>
-      <el-form-item label="活动时间" required>
-        <el-col>
-          <el-form-item prop="date1">
-            <el-date-picker
-              v-model="ruleForm.date1"
-              type="date"
-              placeholder="选择日期"
-              class="w350"
-            />
-          </el-form-item>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col>
-          <el-form-item prop="date2">
-            <el-time-picker v-model="ruleForm.date2" placeholder="选择时间" class="w350" />
-          </el-form-item>
-        </el-col>
+      <el-form-item label="联系电话" prop="mobile">
+        <el-input v-model="ruleForm.mobile" class="w500" />
       </el-form-item>
-      <el-form-item label="即时配送" prop="delivery">
-        <el-switch v-model="ruleForm.delivery" />
+      <el-form-item label="商铺简介" prop="synopsis">
+        <el-input v-model="ruleForm.synopsis" class="w500" />
       </el-form-item>
-      <el-form-item label="活动性质" prop="type">
-        <el-checkbox-group v-model="ruleForm.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type" />
-          <el-checkbox label="地推活动" name="type" />
-          <el-checkbox label="线下主题活动" name="type" />
-          <el-checkbox label="单纯品牌曝光" name="type" />
-        </el-checkbox-group>
+      <el-form-item label="商铺标语" prop="slogan">
+        <el-input v-model="ruleForm.slogan" class="w500" />
       </el-form-item>
-      <el-form-item label="特殊资源" prop="resource">
-        <el-radio-group v-model="ruleForm.resource">
-          <el-radio label="线上品牌商赞助" />
-          <el-radio label="线下场地免费" />
-        </el-radio-group>
+      <el-form-item label="商铺分类" prop="category">
+        <el-cascader v-model="ruleForm.category" :options="categoryOptions" @change="handleChange" />
       </el-form-item>
-      <el-form-item label="活动形式" prop="desc">
-        <el-input v-model="ruleForm.desc" type="textarea" class="w350" />
+      <el-form-item label="配送费" prop="ship_price">
+        <el-input-number
+          v-model="ruleForm.ship_price"
+          controls-position="right"
+          :min="1"
+          :max="20"
+          @change="handleChange"
+        />
+      </el-form-item>
+      <el-form-item label="起送价" prop="send_price">
+        <el-input-number
+          v-model="ruleForm.send_price"
+          controls-position="right"
+          :min="1"
+          :max="20"
+          @change="handleChange"
+        />
+      </el-form-item>
+      <el-form-item label="营业时间" prop="">
+        <el-time-select
+          v-model="ruleForm.start_time"
+          class="mr20"
+          placeholder="起始时间"
+          :picker-options="{
+            start: '08:30',
+            step: '00:15',
+            end: '18:30'}"
+        />
+        <el-time-select
+          v-model="ruleForm.end_time"
+          placeholder="结束时间"
+          :picker-options="{
+            start: '08:30',
+            step: '00:15',
+            end: '18:30',
+            minTime: ruleForm.start_time}"
+        />
+      </el-form-item>
+      <el-form-item label="上传商铺头像" prop="shop_avatar">
+        <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img v-if="ruleForm.shop_avatar" src="ruleForm.shop_avatar" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="上传营业执照" prop="business_license">
+        <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img v-if="ruleForm.business_license" src="ruleForm.business_license" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="上传餐饮许可证" prop="catering_license">
+        <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img v-if="ruleForm.catering_license" src="" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
@@ -68,51 +113,72 @@ export default {
     return {
       ruleForm: {
         name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        address: '',
+        mobile: '',
+        synopsis: '', // 商铺简介
+        slogan: '', // 商铺标语
+        category: [],
+        ship_price: 0,
+        send_price: 0,
+        start_time: '',
+        end_time: '',
+        shop_avatar: '',
+        business_license: '',
+        catering_license: '',
+        longitude: '',
+        latitude: ''
       },
       rules: {
-        name: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' },
-          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        name: [{ required: true, message: '请输入商铺名称', trigger: 'blur' }],
+        address: [
+          { required: true, message: '请选择商铺地址', trigger: 'change' }
         ],
-        region: [
-          { required: true, message: '请选择活动区域', trigger: 'change' }
+        mobile: [
+          { required: true, message: '请填写联系电话', trigger: 'change' }
         ],
-        date1: [
+        synopsis: [
+          { required: true, message: '请填写商铺简介', trigger: 'change' }
+        ],
+        slogan: [
+          { required: true, message: '请填写商铺标语', trigger: 'change' }
+        ],
+        category: [
+          { required: true, message: '请选择商铺分类', trigger: 'change' }
+        ],
+        ship_price: [
+          { required: true, message: '请填写配送费', trigger: 'change' }
+        ],
+        send_price: [
+          { required: true, message: '请填写起送价', trigger: 'change' }
+        ],
+        shop_avatar: [
+          { required: true, message: '请上传商铺头像', trigger: 'change' }
+        ],
+        business_license: [
+          { required: true, message: '请上传营业执照', trigger: 'change' }
+        ],
+        catering_license: [
+          { required: true, message: '请上传餐饮许可证', trigger: 'change' }
+        ],
+        start_time: [
           {
             type: 'date',
             required: true,
-            message: '请选择日期',
+            message: '请选择开始营业时间',
             trigger: 'change'
           }
         ],
-        date2: [
+        end_time: [
           {
             type: 'date',
             required: true,
-            message: '请选择时间',
+            message: '请选择结束营业时间',
             trigger: 'change'
           }
-        ],
-        type: [
-          {
-            type: 'array',
-            required: true,
-            message: '请至少选择一个活动性质',
-            trigger: 'change'
-          }
-        ],
-        resource: [
-          { required: true, message: '请选择活动资源', trigger: 'change' }
-        ],
-        desc: [{ required: true, message: '请填写活动形式', trigger: 'blur' }]
-      }
+        ]
+      },
+      categoryOptions: [
+      ]
     }
   },
   methods: {
@@ -128,7 +194,10 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields()
-    }
+    },
+    handleChange() {},
+    handleAvatarSuccess() {},
+    beforeAvatarUpload() {}
   }
 }
 </script>
@@ -136,12 +205,37 @@ export default {
 <style lang="scss">
 .add-merchants-wrapper {
   width: 100%;
-  height: 100%;
   padding-top: 20px;
+  padding-bottom: 50px;
+  display: flex;
+  justify-content: center;
   background-color: white;
 }
-.w350 {
-  width: 350px;
+.w500 {
+  width: 500px;
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
 
