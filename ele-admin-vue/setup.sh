370bafbee60d00d9d312fd5ca852e3286efbc634
e <2424932515@qq.com>
2020-03-20 23:46:06 +0800

    feat:jenkins集成docker

--- ele-admin-vue/setup.sh
+++ ele-admin-vue/setup.sh
@@ -1,20 +1,28 @@
+###
+ # @Descripttion: 
+ # @version: 
+ # @Author: 笑佛弥勒
+ # @Date: 2020-03-20 23:44:50
+ # @LastEditors: 笑佛弥勒
+ # @LastEditTime: 2020-03-20 23:45:43
+ ###
 #!/usr/bin/env bash
 image_version=`date +%Y%m%d%H%M`;
 # 关闭shop_admin容器
-docker stop shop_admin || true;
+sudo docker stop shop_admin || true;
 # 删除shop_admin容器
-docker rm shop_admin || true;
+sudo docker rm shop_admin || true;
 # 删除shop/admin镜像
-docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
+sudo docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
 # 构建shop/admin:$image_version镜像
-docker build . -t shop/admin:$image_version;
+sudo docker build . -t shop/admin:$image_version;
 # 查看镜像列表
-docker images;
+sudo docker images;
 # 基于shop/admin 镜像 构建一个容器 shop_admin
-docker run -p 9527:80 -d --name shop_admin shop/admin:$image_version;
+sudo docker run -p 9527:80 -d --name shop_admin shop/admin:$image_version;
 # 查看日志
-docker logs shop_admin;
+sudo docker logs shop_admin;
 #删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images -f "dangling=true" -q)
+sudo docker rmi $(docker images -f "dangling=true" -q)
 # 对空间进行自动清理
-docker system prune -a -f
+sudo docker system prune -a -f
