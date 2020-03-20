6641704604aea43a18e6135e17604bf157f56303
chenbin <2424932515@qq.com>
2020-03-20 18:18:56 +0800

    feat:jenkins集成docker

--- ele-admin-vue/push.sh
+++ ele-admin-vue/push.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
-
-echo "正在添加文件..."
-git add .
-echo -n "正在提交备注...，请填写备注（可空）"
-read remarks
-if [ ! -n "$remarks" ];then
-	remarks="常规提交"
-fi
-git commit -m "$remarks"
-echo "正在开始提交代码..."
-git push
-echo "代码提交成功，正在关闭..."
+#!/usr/bin/env bash
+image_version=`date +%Y%m%d%H%M`;
+# 关闭shop_admin容器
+docker stop shop_admin || true;
+# 删除shop_admin容器
+docker rm shop_admin || true;
+# 删除shop/admin镜像
+docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
+# 构建shop/admin:$image_version镜像
+docker build . -t shop/admin:$image_version;
+# 查看镜像列表
+docker images;
+# 基于shop/admin 镜像 构建一个容器 shop_admin
+docker run -p 9527:80 -d --name shop_admin shop/admin:$image_version;
+# 查看日志
+docker logs shop_admin;
+#删除build过程中产生的镜像    #docker image prune -a -f
+docker rmi $(docker images -f "dangling=true" -q)
+# 对空间进行自动清理
+docker system prune -a -f
