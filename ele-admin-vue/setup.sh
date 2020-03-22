eddbde8817e8daa1d3bc1f5c75504d2866bd109f
e <2424932515@qq.com>
2020-03-22 14:28:24 +0800

    fix

--- ele-admin-vue/setup.sh
+++ ele-admin-vue/setup.sh
@@ -9,12 +9,12 @@ docker rm shop_admin || true;
 docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
 # 构建shop/admin:$image_version镜像
 echo "开始docker build命令"
-docker build . -t shop/admin:$image_version;
+docker build . -t shop/admin;
 # 查看镜像列表
 echo "开始查看镜像列表"
 docker images;
 # 基于shop/admin 镜像 构建一个容器 shop_admin
-docker run -p 9527:80 -d --name shop_admin shop/admin:$image_version;
+docker run -p 9527:80 -d --name shop_admin shop/admin;
 # 查看日志
 docker logs shop_admin;
 #删除build过程中产生的镜像    #docker image prune -a -f
