0c47065fc7433a5d9050b53162019d7cbcbd3f95
chenbin <2424932515@qq.com>
2020-03-26 21:02:10 +0800

    feat:修改路由

--- ele-admin-vue/src/services/index.js
+++ ele-admin-vue/src/services/index.js
@@ -2,97 +2,97 @@ import { get, post } from 'common/js/request'
 
 // 登录
 const login = params => {
-  return post('/admin/login', params)
+  return post('/api/admin/login', params)
 }
 
 // 获取数据总览
 const getAllData = params => {
-  return get('/admin/totalData', params)
+  return get('/api/admin/totalData', params)
 }
 
 // 创建商户
 const createMerchants = params => {
-  return post('/merchants/createMerchants', params)
+  return post('/api/merchants/createMerchants', params)
 }
 
 // 获取分类
 const getCategory = params => {
-  return get('/admin/getShopCategory', params)
+  return get('/api/admin/getShopCategory', params)
 }
 
 // 创建食品分类
 const createFoodCategory = params => {
-  return post('/food/createFoodCategory', params)
+  return post('/api/food/createFoodCategory', params)
 }
 
 // 获取食品分类
 const getCategoryByPid = params => {
-  return get('/food/getCategoryByPid', params)
+  return get('/api/food/getCategoryByPid', params)
 }
 
 // 新增食品
 const createdFood = params => {
-  return post('/food/createdFood', params)
+  return post('/api/food/createdFood', params)
 }
 
 // 商家列表
 const getMerchantsByPage = params => {
-  return get('/merchants/getMerchantsByPage', params)
+  return get('/api/merchants/getMerchantsByPage', params)
 }
 
 // 删除商家
 const deleteMerchants = params => {
-  return post('/merchants/deleteMerchants', params)
+  return post('/api/merchants/deleteMerchants', params)
 }
 
 // 获取单个商家
 const getMerchantsById = params => {
-  return get('/merchants/getMerchantsById', params)
+  return get('/api/merchants/getMerchantsById', params)
 }
 
 // 更新商家
 const updateMerchants = params => {
-  return post('/merchants/updateMerchants', params)
+  return post('/api/merchants/updateMerchants', params)
 }
 
 // 获取食品列表
 const findFoodByPage = params => {
-  return get('/food/findFoodByPage', params)
+  return get('/api/food/findFoodByPage', params)
 }
 
 // 删除食品
 const deleteFood = params => {
-  return get('/food/deleteFood', params)
+  return get('/api/food/deleteFood', params)
 }
 
 // 获取单个食品详情
 const getFoodById = params => {
-  return get('/food/getFoodById', params)
+  return get('/api/food/getFoodById', params)
 }
 
 // 更新食品属性
 const updatedFood = params => {
-  return post('/food/updatedFood', params)
+  return post('/api/food/updatedFood', params)
 }
 
 // 获取当前用户信息
 const getCurrentAdmin = params => {
-  return get('/admin/getCurrentAdmin', params)
+  return get('/api/admin/getCurrentAdmin', params)
 }
 
 // 获取管理员列表
 const findAdminByPage = params => {
-  return get('/admin/findAdminByPage', params)
+  return get('/api/admin/findAdminByPage', params)
 }
 
 // 获取用户列表
 const getUserList = params => {
-  return get('/user/getUserList', params)
+  return get('/api/user/getUserList', params)
 }
 
 // 获取用户列表
 const isLogin = params => {
-  return get('/user/isLogin', params)
+  return get('/api/user/isLogin', params)
 }
 export default {
   login,
--- ele-admin-vue/vue.config.js
+++ ele-admin-vue/vue.config.js
@@ -31,22 +31,7 @@ module.exports = {
     https: false, // 使用https提供服务
     // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
     proxy: {
-      '/admin/*': {
-        target: 'http://127.0.0.1:7001',
-        changeOrigin: true,
-        secure: false
-      },
-      '/merchants/*': {
-        target: 'http://127.0.0.1:7001',
-        changeOrigin: true,
-        secure: false
-      },
-      '/food/*': {
-        target: 'http://127.0.0.1:7001',
-        changeOrigin: true,
-        secure: false
-      },
-      '/user/*': {
+      '/api/*': {
         target: 'http://127.0.0.1:7001',
         changeOrigin: true,
         secure: false
